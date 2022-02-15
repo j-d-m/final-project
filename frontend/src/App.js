c75c3a674d1b9b3efb84312bf20c2d872e0ee0f0
KrishnaA01 <mahato77@hotmail.com>
2022-02-15 16:25:16 +0100

    navbar first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.scss';
+import Navbar from './components/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar />
+    </div>
+  );
+}
+
+export default App;
