98ce23774a1db41b00d0455e9edb54b1a5882501
KrishnaA01 <mahato77@hotmail.com>
2022-02-20 17:30:34 +0100

    navbar changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Women from './components/Women'
+
+import App from "./App";
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  
+  <Router>
+  <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/women' element={<Women/>} />
+  </Routes>
+</Router>,
+  
+  document.getElementById("root")
 );
