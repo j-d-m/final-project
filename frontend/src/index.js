65cb3b51792e226ec61edb9e5ad9682bf0bbe06f
Joshua Moir <j.moir.nz@gmail.com>
2022-02-21 22:45:38 +0100

    added context api-first working context && Apollo provider to the index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,22 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-
-
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+//let's initialize an ApolloClient instance.
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
