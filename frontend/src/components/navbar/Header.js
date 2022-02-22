790082489fc12c6c16ef6c6571a2b5f323ae4ccd
KrishnaA01 <mahato77@hotmail.com>
2022-02-22 11:28:05 +0100

    logo updated

--- frontend/src/components/navbar/Header.js
+++ frontend/src/components/navbar/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
-import logo from './logo.svg'
+import avatar from '../../assets/img/avatar.svg';
+import logo from '../../assets/img/logo.svg';
 import { AiOutlineLogin } from 'react-icons/ai';
 import { HiOutlineOfficeBuilding } from 'react-icons/hi';
 import { BsPersonFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import '../../utils/styles/navbar.scss'
 
 
 
@@ -20,27 +22,31 @@ export default function Header() {
           height="40"
           className="d-inline-block align-top"
         />{' '}
-      FreesBee GmbH
+      Staff Room
     </Navbar.Brand>
 
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
 
-      <Nav className="mx-auto TopHeader">
-      <Nav.Link as={Link} to="/home" > Home </Nav.Link>
-        <NavDropdown title={<span><AiOutlineLogin/> Login </span>}  id="basic-nav-dropdown">
+      <Nav className="mx-auto TopHeader ">
+      <Nav.Link as={Link} to="/home" > Jobs </Nav.Link>
+
+        <NavDropdown title={<span><AiOutlineLogin/> Freelancers </span>}  id="basic-nav-dropdown">
           <NavDropdown.Item as={Link} to="/freelancer-login" >Freelancer Login</NavDropdown.Item>
-          <NavDropdown.Item as={Link} to="/freelancer-profile" >Freelancer Profile</NavDropdown.Item>
-          <NavDropdown.Item as={Link} to="/company-login" >Login for Employer</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/home" > Freelancer SignUp</NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link as={Link} to="/freelancer-profile" > <BsPersonFill/></Nav.Link>
 
-       <Nav.Link as={Link} to="/company-profile"> Company Profile </Nav.Link>
-
-        <Nav.Link as={Link} to="/company-login"> <HiOutlineOfficeBuilding/> For Employers</Nav.Link>
+        <span className="DisappearI text-light mt-1"> <BsPersonFill/></span>
 
 
-       
+        <NavDropdown title={<span><AiOutlineLogin/> Employer </span>}  id="basic-nav-dropdown">
+          <NavDropdown.Item as={Link} to="/freelancer-login" >Employer Login</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/home" > Employer SignUp</NavDropdown.Item>
+        </NavDropdown>
+
+        <span className="DisappearI text-light mt-1" > <HiOutlineOfficeBuilding/></span>
+         <img className="ms-5 DisappearI" width='50' height='50' src={avatar} alt="" />{' '}
+ 
       </Nav>
     </Navbar.Collapse>
   </Container>
