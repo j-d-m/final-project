b54feb4db1e669cb2a8c37d241ca2f2b15098416
ademten82 <ademten7@gmail.com>
2022-02-22 08:50:59 +0100

    FRONTEND-graphQL folder completed

--- frontend/src/graphQL/Mutations.js
+++ frontend/src/graphQL/Mutations.js
@@ -1,4 +1,5 @@
 import { gql } from "@apollo/client";
+//COMPANY MUTATIONS
 //add company
 export const CREATE_COMPANY_MUTATION = gql`
   mutation AddCompany(
@@ -25,6 +26,64 @@ export const CREATE_COMPANY_MUTATION = gql`
     ) {
       company_name
       owner_name
+      company_type
+      address
+      phone
+      email
+      password
+      repeatPassword
+      description
+    }
+  }
+`;
+//login as company
+export const COMPANY_LOGIN = gql`
+  mutation LoginCompany($email: String!, $password: String!) {
+    loginCompany(email: $email, password: $password) {
+      companyId
+      token
+      tokenExpiration
+    }
+  }
+`;
+//Delete Company
+export const DELETE_COMPANY = gql`
+  mutation DeleteCompany($deleteCompanyId: ID) {
+    deleteCompany(id: $deleteCompanyId) {
+      company_name
+    }
+  }
+`;
+//update Company
+export const UPDATE_COMPANY = gql`
+  mutation UpdateCompany(
+    $updateCompanyId: ID!
+    $companyName: String
+    $ownerName: String
+    $avatar: String
+    $companyType: String
+    $address: String
+    $phone: String
+    $email: String
+    $password: String
+    $description: String
+  ) {
+    updateCompany(
+      id: $updateCompanyId
+      company_name: $companyName
+      owner_name: $ownerName
+      avatar: $avatar
+      company_type: $companyType
+      address: $address
+      phone: $phone
+      email: $email
+      password: $password
+      description: $description
+    ) {
+      id
+      company_name
+      owner_name
+      avatar
       company_type
       address
       phone
@@ -73,13 +132,116 @@ export const USER_LOGIN = gql`
     }
   }
 `;
-//login as company
-export const COMPANY_LOGIN = gql`
-  mutation LoginCompany($email: String!, $password: String!) {
-    loginCompany(email: $email, password: $password) {
-      companyId
-      token
-      tokenExpiration
+//Delete User
+export const DELETE_USER = gql`
+  mutation DeleteUser($deleteUserId: ID) {
+    deleteUser(id: $deleteUserId) {
+      id
+      first_name
+      last_name
+    }
+  }
+`;
+//Delete User
+export const UPDATE_USER = gql`
+  mutation UpdateUser(
+    $updateUserId: ID!
+    $firstName: String
+    $lastName: String
+    $avatar: String
+    $email: String
+    $password: String
+    $phone: String
+    $hourlyRate: Int
+    $description: String
+  ) {
+    updateUser(
+      id: $updateUserId
+      first_name: $firstName
+      last_name: $lastName
+      avatar: $avatar
+      email: $email
+      password: $password
+      phone: $phone
+      hourly_rate: $hourlyRate
+      description: $description
+    ) {
+      id
+      first_name
+      last_name
+      avatar
+      email
+      phone
+      password
+      repeatPassword
+      hourly_rate
+      description
+    }
+  }
+`;
+//Job Mutations
+export const CREATE_JOB = gql`
+  mutation AddJob(
+    $jobTitle: String!
+    $companyName: String!
+    $date: String!
+    $numOfPeopleNeeded: Int!
+    $jobDescription: String!
+    $createdBy: ID!
+  ) {
+    addJob(
+      job_Title: $jobTitle
+      company_Name: $companyName
+      date: $date
+      num_of_people_needed: $numOfPeopleNeeded
+      job_description: $jobDescription
+      created_by: $createdBy
+    ) {
+      id
+      job_Title
+      company_Name
+      date
+      num_of_people_needed
+      job_description
+    }
+  }
+`;
+
+//Delete User
+export const DELETE_JOB = gql`
+  mutation DeleteJob($deleteJobId: ID) {
+    deleteJob(id: $deleteJobId) {
+      job_Title
+    }
+  }
+`;
+
+//Delete User
+export const UPDATE_JOB = gql`
+  mutation UpdateJob(
+    $updateJobId: ID!
+    $jobTitle: String
+    $companyName: String
+    $date: String
+    $numOfPeopleNeeded: Int
+    $jobDescription: String
+    $createdBy: String
+  ) {
+    updateJob(
+      id: $updateJobId
+      job_Title: $jobTitle
+      company_Name: $companyName
+      date: $date
+      num_of_people_needed: $numOfPeopleNeeded
+      job_description: $jobDescription
+      created_by: $createdBy
+    ) {
+      id
+      job_Title
+      company_Name
+      date
+      num_of_people_needed
+      job_description
     }
   }
 `;
--- frontend/src/graphQL/Queries.js
+++ frontend/src/graphQL/Queries.js
@@ -50,3 +50,45 @@ export const GET_ONE_COMPANY = gql`
     }
   }
 `;
+export const GET_COMPANIES = gql`
+  query GetCompanies {
+    getCompanies {
+      id
+      company_name
+      owner_name
+      avatar
+      company_type
+      address
+      phone
+      email
+      password
+      repeatPassword
+      description
+    }
+  }
+`;
+//job queries
+export const GET_ONE_JOB = gql`
+  query GetOneJob($getOneJobId: ID) {
+    getOneJob(id: $getOneJobId) {
+      id
+      job_Title
+      company_Name
+      date
+      num_of_people_needed
+      job_description
+    }
+  }
+`;
+export const GET_JOBS = gql`
+  query GetJobs {
+    getJobs {
+      id
+      job_Title
+      company_Name
+      date
+      num_of_people_needed
+      job_description
+    }
+  }
+`;
