ebb23bf43d2085d6c73fc48f21b4463d931da4a7
Joshua Moir <j.moir.nz@gmail.com>
2022-02-26 21:39:02 +0100

    refactored the company create-job page

--- frontend/src/components/company/company-profile/CompanyProfile.js
+++ frontend/src/components/company/company-profile/CompanyProfile.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from "react";
+import "../../../styles/_colors.scss";
+import "../../../styles/_fonts.scss";
+import { gql, useQuery } from "@apollo/client";
+import { GET_ONE_COMPANY } from "../../../graphQL/Queries";
+import { UPDATE_COMPANY } from "../../../graphQL/Mutations";
 
 export default function CompanyProfile() {
-    return (
-        <div
-            style={{ height: '100%', width: '100vw', backgroundColor: '#C9cbfb' }}
-        >
-            <h1
-                style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '75vh' }}
-            >
-                Company Profile (placeholder)</h1>
-        </div >
-    )
+  return (
+    <div style={{ height: "100%", width: "100vw", backgroundColor: "#C9cbfb" }}>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "75vh",
+        }}
+      >
+        Company Profile (placeholder)
+      </h1>
+    </div>
+  );
 }
--- frontend/src/components/company/company-profile/company-create-job/CreateJob.js
+++ frontend/src/components/company/company-profile/company-create-job/CreateJob.js
@@ -1,47 +1,54 @@
 import { useMutation } from "@apollo/client";
-import React, { useState } from "react";
+import React, { useContext } from "react";
+import { MyContext } from "../../../../Context/Context";
 import { CREATE_JOB } from "../../../../graphQL/Mutations";
-
+import Swal from "sweetalert2";
+import { GET_JOBS } from "../../../../graphQL/Queries";
 function CreateJob() {
-  const [textInput, setTextInput] = useState("");
-
-  let jobTitle, date, numOfPeopleNeeded, jobDescription, companyName, createdBy;
-
-  const [addJob, { data, loading, error }] = useMutation(CREATE_JOB);
-
-  const message = (e) => {
-    e.preventDefault();
-    setTextInput(e.target.value);
-  };
-
+  const { companyLoginData } = useContext(MyContext);
+  const [addJob, { data, loading, error }] = useMutation(CREATE_JOB, {
+    refetchQueries: [{ query: GET_JOBS }],
+    awaitRefetchQueries: true,
+  });
   const addJobProfile = (e) => {
     e.preventDefault();
-    console.log(e.target);
     addJob({
       variables: {
-        jobTitle: jobTitle.value,
-        date: date.value,
-        numOfPeopleNeeded: numOfPeopleNeeded.value,
-        jobDescription: textInput,
-        // companyName: companyName.value,
-        // createdBy:createdBy.value,
+        jobTitle: e.target.jobTitle.value,
+        startDate: e.target.startDate.value,
+        endDate: e.target.endDate.value,
+        numOfPeopleNeeded: Number(e.target.numOfPeopleNeeded.value),
+        jobDescription: e.target.jobDescription.value,
+        createdBy: companyLoginData.companyId,
       },
+    }).then((res) => {
+      if (res.data) {
+        Swal.fire({
+          position: "top",
+          icon: "success",
+          title: "Job created successfully",
+          showConfirmButton: false,
+          timer: 1000,
+        });
+      }
+      if (error) {
+        Swal.fire({
+          position: "top",
+          icon: "error",
+          title: "something went wrong",
+          showConfirmButton: false,
+          timer: 1000,
+        });
+      }
     });
   };
-
-  console.log(textInput);
-
-  if (loading) return <p> Loading...</p>;
-  if (error) return <p> there is error</p>;
-  console.log(data, error);
-
+  console.log(data);
   return (
     <div>
       <form onSubmit={addJobProfile} className="w-75 m-auto">
         <div className="mb-3">
           <label className="form-label">Job Title</label>
           <input
-            ref={(value) => (jobTitle = value)}
             name="jobTitle"
             type="text"
             className="form-control"
@@ -49,18 +56,16 @@ function CreateJob() {
           />
         </div>
         <div className="mb-3">
-          <label className="form-label">Date</label>
-          <input
-            ref={(value) => (date = value)}
-            name="date"
-            type="date"
-            className="form-control"
-          />
+          <label className="form-label">Start Date</label>
+          <input name="startDate" type="date" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">End Date</label>
+          <input name="endDate" type="date" className="form-control" />
         </div>
         <div className="mb-3">
           <label className="form-label">number of people</label>
           <input
-            ref={(value) => (numOfPeopleNeeded = value)}
             name="numOfPeopleNeeded"
             type="number"
             className="form-control"
@@ -68,7 +73,7 @@ function CreateJob() {
         </div>
         <div className="form-outline">
           <textarea
-            onChange={message}
+            name="jobDescription"
             className="form-control"
             rows="8"
             cols="60"
@@ -82,5 +87,4 @@ function CreateJob() {
     </div>
   );
 }
-
 export default CreateJob;
