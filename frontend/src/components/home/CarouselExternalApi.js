d29295b50976639646a9f1f61d7f479b35ff1be6
Joshua Moir <j.moir.nz@gmail.com>
2022-02-28 23:35:51 +0100

    added styled components to the package json

--- frontend/src/components/home/CarouselExternalApi.js
+++ frontend/src/components/home/CarouselExternalApi.js
@@ -1,16 +1,15 @@
 //Native imports
-import React, { useEffect, useState, Component } from 'react'
+import React, { useEffect, useState } from "react";
 
 //External imports
-import Button from 'react-bootstrap/Button'
-import Carousel from 'react-elastic-carousel';
-import moment from 'moment'
+import Button from "react-bootstrap/Button";
+import Carousel from "react-elastic-carousel";
+import moment from "moment";
 
 //Internal imports
 // import Adzuna from "../../services/external-api/Adzuna";
 import API_URL from "../../services/external-api/Adzuna";
-import '../../styles/carousel.scss';
-
+import "../../styles/carousel.scss";
 
 const breakPoints = [
   { width: 1, itemsToShow: 1, itemsToScroll: 1 },
@@ -20,7 +19,6 @@ const breakPoints = [
 ];
 
 function CarouselExternalApi() {
-
   const [apiJobs, setApiJobs] = useState([]);
 
   useEffect(() => {
@@ -37,40 +35,51 @@ function CarouselExternalApi() {
     fetchData();
   }, []);
 
-  let charLimitCompany = 30
-  let charLimitTitle = 30
+  let charLimitCompany = 30;
+  let charLimitTitle = 30;
 
   return (
-
     <div className="jobs-carousel">
       <div className="carousel-wrapper">
         <Carousel breakPoints={breakPoints}>
           {apiJobs.map((job) => (
-          < div className = 'carousel-card' key = { job.id } >
-          <h5> {(job.title).slice(0, charLimitTitle) + ((job.title).length > charLimitTitle ? "..." : "")}</h5>
-              {/* <h4>{(job.company.display_name).slice(0, charLimitCompany) + ((job.company.display_name).length > charLimitCompany ? "..." : "")}</h4> */ }
-            <p p > { job.location.display_name }</p>
-        <p>posted {moment(job.created).fromNow()}</p>
-        <p>by <strong> {(job.company.display_name).slice(0, charLimitCompany) + ((job.company.display_name).length > charLimitCompany ? "..." : "")} </strong></p>
+            <div className="carousel-card" key={job.id}>
+              <h5>
+                {" "}
+                {job.title.slice(0, charLimitTitle) +
+                  (job.title.length > charLimitTitle ? "..." : "")}
+              </h5>
+              {/* <h4>{(job.company.display_name).slice(0, charLimitCompany) + ((job.company.display_name).length > charLimitCompany ? "..." : "")}</h4> */}
+              <p p> {job.location.display_name}</p>
+              <p>posted {moment(job.created).fromNow()}</p>
+              <p>
+                by{" "}
+                <strong>
+                  {" "}
+                  {job.company.display_name.slice(0, charLimitCompany) +
+                    (job.company.display_name.length > charLimitCompany
+                      ? "..."
+                      : "")}{" "}
+                </strong>
+              </p>
 
-        <div className="text-center">
-          <Button variant="secondary" size="md"
-            href={job.redirect_url}
-            target="_blank"
-          >
-            Accept Job
-          </Button>
-        </div>
-      </div>
+              <div className="text-center">
+                <Button
+                  variant="secondary"
+                  size="md"
+                  href={job.redirect_url}
+                  target="_blank"
+                >
+                  Accept Job
+                </Button>
+              </div>
+            </div>
           ))}
-    </Carousel>
-      </div >
-    <hr className="separator" />
-
-    </div >
-  )
+        </Carousel>
+      </div>
+      <hr className="separator" />
+    </div>
+  );
 }
 
-
-export default CarouselExternalApi
-
+export default CarouselExternalApi;
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,38 +1,37 @@
 //Native imports
 import React, { useContext, useEffect, useState } from "react";
 
-
 //External imports
 import { useQuery } from "@apollo/client";
-import { logMissingFieldErrors } from "@apollo/client/core/ObservableQuery";
-import Swal from "sweetalert2";
 
+import Swal from "sweetalert2";
 
 // Internal imports
 import { MyContext } from "../../Context/Context";
 import { GET_JOBS } from "../../graphQL/Queries";
-import '../../styles/home.scss';
+import "../../styles/home.scss";
 import exclamation from "../../assets/img/exclamation.ico";
-import CarouselInternalApi from "./CarouselInternalApi";
+import CarouselInternalApi from "./CarouselExternalApi";
 import CarouselExternalApi from "./CarouselExternalApi";
-
 
 export default function Home() {
   const { data, loading, error } = useQuery(GET_JOBS);
 
-  const { isTitleFilter, setIsTitleFilter, inputValue, setInputValue } = useContext(MyContext)
-
+  const { isTitleFilter, setIsTitleFilter, inputValue, setInputValue } =
+    useContext(MyContext);
 
   const searchHandler = (e) => {
-    e.preventDefault()
-    let inputTitleValue = e.target.searchJobTitle.value
+    e.preventDefault();
+    let inputTitleValue = e.target.searchJobTitle.value;
 
-    const filterTitle = data.getJobs.filter((item) => item.job_Title === inputTitleValue);
+    const filterTitle = data.getJobs.filter(
+      (item) => item.job_Title === inputTitleValue
+    );
     // console.log(filterTitle[0].job_description);
 
     if (filterTitle.length > 0) {
-      setInputValue(filterTitle)
-      setIsTitleFilter(true)
+      setInputValue(filterTitle);
+      setIsTitleFilter(true);
     } else {
       Swal.fire({
         position: "top",
@@ -45,8 +44,7 @@ export default function Home() {
         timer: 3000,
       });
     }
-
-  }
+  };
 
   if (loading) {
     return (
@@ -71,77 +69,83 @@ export default function Home() {
       <section className="home-container">
         <div className="banner-container">
           <div className="search-fields">
-            <form onSubmit={searchHandler} >
-              <input name="searchJobTitle" type="text" placeholder="job title... " />
-              <input className="search-button" type="submit" value="Search Jobs" />
+            <form onSubmit={searchHandler}>
+              <input
+                name="searchJobTitle"
+                type="text"
+                placeholder="job title... "
+              />
+              <input
+                className="search-button"
+                type="submit"
+                value="Search Jobs"
+              />
             </form>
           </div>
         </div>
       </section>
 
-      {isTitleFilter ? inputValue.map((job) => {
-        return (
-          <div key={job.id} className=" CardDiv ">
-            <div className="card-body">
-              <img
-                src={`https://source.unsplash.com/1600x900/?${job.job_Title}`}
-                alt="img"
-              />
+      {isTitleFilter
+        ? inputValue.map((job) => {
+            return (
+              <div key={job.id} className=" CardDiv ">
+                <div className="card-body">
+                  <img
+                    src={`https://source.unsplash.com/1600x900/?${job.job_Title}`}
+                    alt="img"
+                  />
 
-              <p>Title : {job.job_Title}</p>
-              <p>Description : {job.job_description}</p>
-              <p>Number Needed :{job.num_of_people_needed}</p>
-              <p>issued at :{job.issued_At}</p>
-              <div>
-                <h4>created by : {job.created_by.company_Name}</h4>
-                <p>email : {job.created_by.email}</p>
+                  <p>Title : {job.job_Title}</p>
+                  <p>Description : {job.job_description}</p>
+                  <p>Number Needed :{job.num_of_people_needed}</p>
+                  <p>issued at :{job.issued_At}</p>
+                  <div>
+                    <h4>created by : {job.created_by.company_Name}</h4>
+                    <p>email : {job.created_by.email}</p>
+                  </div>
+                  <div className="text-center">
+                    <input
+                      type="button"
+                      value="Accept Job"
+                      className="btn btn-secondary"
+                    />
+                  </div>
+                </div>
               </div>
-              <div className="text-center">
-                <input
-                  type="button"
-                  value="Accept Job"
-                  className="btn btn-secondary"
+            );
+          })
+        : data.getJobs.map((job) => {
+            return (
+              <div key={job.id} className=" CardDiv ">
+                <div className="card-body">
+                  <img
+                    src={`https://source.unsplash.com/1600x900/?${job.job_Title}`}
+                    alt="img"
+                  />
 
-                />
+                  <p>Title : {job.job_Title}</p>
+                  <p>Description : {job.job_description}</p>
+                  <p>Number Needed :{job.num_of_people_needed}</p>
+                  <p>issued at :{job.issued_At}</p>
+                  <div>
+                    <h4>created by : {job.created_by.company_Name}</h4>
+                    <p>email : {job.created_by.email}</p>
+                  </div>
+                  <div className="text-center">
+                    <input
+                      type="button"
+                      value="Accept Job"
+                      className="btn btn-secondary"
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        );
-      }) : data.getJobs.map((job) => {
-        return (
-          <div key={job.id} className=" CardDiv ">
-            <div className="card-body">
-              <img
-                src={`https://source.unsplash.com/1600x900/?${job.job_Title}`}
-                alt="img"
-              />
-
-              <p>Title : {job.job_Title}</p>
-              <p>Description : {job.job_description}</p>
-              <p>Number Needed :{job.num_of_people_needed}</p>
-              <p>issued at :{job.issued_At}</p>
-              <div>
-                <h4>created by : {job.created_by.company_Name}</h4>
-                <p>email : {job.created_by.email}</p>
-              </div>
-              <div className="text-center">
-                <input
-                  type="button"
-                  value="Accept Job"
-                  className="btn btn-secondary"
-
-                />
-              </div>
-            </div>
-          </div>
-        );
-      })}
-      <div className="jobs-combo-box" >
+            );
+          })}
+      <div className="jobs-combo-box">
         <CarouselInternalApi />
         <CarouselExternalApi />
       </div>
     </div>
   );
 }
-
-
