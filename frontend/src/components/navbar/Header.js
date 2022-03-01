267d0b4c51c99f68826ab60edc5978179bdb1fe3
anass-knene <anas.knene@hotmail.com>
2022-03-01 23:46:08 +0100

    fix conflicts

--- frontend/src/components/navbar/Header.js
+++ frontend/src/components/navbar/Header.js
@@ -37,15 +37,7 @@ export default function Header() {
             className="d-inline-block align-top"
           />
           STAFF ROOM{" "}
-          <p className="slogan-text">
-            {" "}
-            GASTRONOMY JOBS / PEOPLE WHEN YOU NEED{" "}
-          </p>
-          <h1 className="logo-text"> STAFF ROOM</h1>{" "}
-          <p className="slogan-text">
-            {" "}
-            GASTRONOMY JOBS / PEOPLE WHEN YOU NEED{" "}
-          </p>
+          <p className="slogan-text"> GASTRONOMY JOBS / PEOPLE WHEN YOU NEED</p>
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
