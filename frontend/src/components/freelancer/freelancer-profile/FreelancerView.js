cb303bd08cd32f486c4b2e9609d7941604b35ae4
anass-knene <anas.knene@hotmail.com>
2022-03-04 23:50:56 +0100

    navigate company to  freelancer-view components + contact form

--- frontend/src/components/freelancer/freelancer-profile/FreelancerView.js
+++ frontend/src/components/freelancer/freelancer-profile/FreelancerView.js
@@ -1,11 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { MyContext } from "../../../Context/Context";
+import { ImHeart } from "react-icons/im";
+import { HiOutlineMail, HiOutlineMailOpen } from "react-icons/hi";
+import { Link } from "react-scroll";
 import "../../../styles/_freelancerView.scss";
 export default function FreelancerView() {
+  /*   1- in this components we need to add email js 
+       in order to send message from company to freelancer 
+       2- we need to add functionality to favorite icon 
+       */
   const { freelancerFind } = useContext(MyContext);
+  const [showContactForm, setShowContactForm] = useState(false);
+  let { first_name, last_name, hourly_rate, email, avatar } = freelancerFind;
+  console.log(showContactForm);
   return (
     <div className="freelancerView">
-      <div className="container d-flex justify-content-center align-items-center">
+      <div className="freelancerViewContainer d-flex justify-content-center align-items-center ">
         <div className="card">
           <div className="upper">
             <img
@@ -17,7 +27,7 @@ export default function FreelancerView() {
           <div className="user text-center">
             <div className="profile">
               <img
-                src={freelancerFind.avatar}
+                src={avatar}
                 alt="img"
                 className="rounded-circle"
                 width="80"
@@ -25,24 +35,68 @@ export default function FreelancerView() {
             </div>
           </div>
           <div className="mt-5 text-center">
-            <h4 className="mb-0">{`${freelancerFind.first_name} ${freelancerFind.last_name}`}</h4>
-            <span className="text-muted d-block mb-2">
-              {freelancerFind.email}
-            </span>
-            <button className="btn btn-primary btn-sm follow">Follow</button>
-            <div className="d-flex justify-content-between align-items-center mt-4 px-4">
-              <div className="stats">
-                <h6 className="mb-0">Hourly_rate</h6>
-                <span>{freelancerFind.hourly_rate}</span>
-              </div>
-              <div className="stats">
-                <h6 className="mb-0">Projects</h6> <span>142</span>
-              </div>
-              <div className="stats">
-                <h6 className="mb-0">Ranks</h6> <span>129</span>
-              </div>
+            <h4 className="mb-0">{`${first_name} ${last_name}`}</h4>
+            <span className="text-muted d-block mb-2">{email}</span>
+
+            <ImHeart className="iconHeart text-danger" size="35px" />
+
+            <div className="stats mt-2">
+              <h6 className="mb-0">Hourly_rate</h6>
+              <span>{hourly_rate}</span>
             </div>
           </div>
+          <div className="contactMainBtn text-center">
+            <Link
+              to="submit"
+              spy={true}
+              smooth={true}
+              offset={320}
+              duration={200}
+              delay={500}
+            >
+              {showContactForm ? (
+                <HiOutlineMail
+                  onClick={() => setShowContactForm(false)}
+                  size="35px"
+                  style={{ cursor: "pointer" }}
+                />
+              ) : (
+                <HiOutlineMailOpen
+                  onClick={() => setShowContactForm(true)}
+                  size="35px"
+                  style={{ cursor: "pointer" }}
+                />
+              )}
+            </Link>
+          </div>
+          <form
+            id="submit"
+            className={
+              showContactForm
+                ? "d-none "
+                : "d-block  animate__animated animate__fadeInDownBig"
+            }
+          >
+            <h1 className="text-danger">Contact Me</h1>
+            <input
+              name="name"
+              type="text"
+              className="feedback-input"
+              placeholder="Name"
+            />
+            <input
+              name="email"
+              type="text"
+              className="feedback-input"
+              placeholder="Email"
+            />
+            <textarea
+              name="text"
+              className="feedback-input"
+              placeholder="Comment"
+            ></textarea>
+            <input type="submit" value="SUBMIT" className="bg-danger" />
+          </form>
         </div>
       </div>
     </div>
--- frontend/src/components/home/FreelancerHome.js
+++ frontend/src/components/home/FreelancerHome.js
@@ -1,13 +1,11 @@
 import { useQuery } from "@apollo/client";
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { GET_USERS } from "../../graphQL/Queries";
 import "../../styles/_freelancerHome.scss";
-import { FaHardHat } from "react-icons/fa";
 import { MyContext } from "../../Context/Context";
 import { useNavigate } from "react-router-dom";
 
 export default function FreelancerHome() {
-  const [favIconColor, setFavIconColor] = useState(false);
   const navigate = useNavigate();
   const { setFreelancerFind } = useContext(MyContext);
   const { loading, error, data } = useQuery(GET_USERS);
@@ -18,38 +16,36 @@ export default function FreelancerHome() {
     console.log(error);
     return <p>error:</p>;
   }
-  const addFavorite = (id) => {
-    console.log(id);
+
+  const contactFreelancer = (id) => {
     let findUser = data.getUsers.find((freelancer) => freelancer.id === id);
     setFreelancerFind(findUser);
-
-    setFavIconColor(!favIconColor);
+    navigate("/freelancer-view");
   };
 
-  console.log(data);
   return (
     <div className="freelancer">
       {data &&
         data.getUsers.map((user) => {
           return (
-            <section className="CardUser">
+            <section
+              className="CardUser animate__animated animate__backInLeft"
+              key={user.id}
+            >
+              <img src={user.avatar} alt="img" width="150px" height="150px" />
               <div className="bodyCard">
                 <h2 className="name">{`${user.first_name} ${user.last_name}`}</h2>
-                <img src={user.avatar} alt="img" width="50px" />
                 <p> {user.email}</p>
-              </div>
-              <div className="BtnDiv">
                 <input
                   type="button"
                   value="contact"
                   className="Btn"
-                  onClick={() => navigate("/freelancer-view")}
+                  onClick={() => contactFreelancer(user.id)}
                 />
-
-                <FaHardHat
-                  className={favIconColor ? "text-danger" : "fa-favorite"}
-                  onClick={() => addFavorite(user.id)}
-                />
+              </div>
+              <div className="description">
+                <p>Description</p>
+                <p>{user.description}</p>
               </div>
             </section>
           );
